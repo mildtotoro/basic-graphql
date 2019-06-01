6f60f5860b1902d69c867236ec5b6f785d96e6b4
mildtotoro <pimrampha.m@gmail.com>
2019-06-01 11:15:11 +0700

    Integrate connect to database by prisms-client-lib

--- src/index.js
+++ src/index.js
@@ -1,68 +1,56 @@
 const { GraphQLServer } = require('graphql-yoga');
 // import cloneDeep from 'lodash/cloneDeep';
 const cloneDeep = require('lodash/cloneDeep');
+const { prisma } = require('./generated/prisma-client');
 
-let links = [{
-  id: 'link-0',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-},
-{
-  id: 'link-1',
-  url: 'www.howtographql.com',
-  description: 'Fullstack tutorial for GraphQL'
-}]
-
-let idCount = links.length
-const resolvers = { 
+const resolvers = {
   Query: {
-    info: () => "Test",
-    feed: () => links,
+    info: () => "This is the API of a Basic Graphql",
+    feed: (root, args, context, info) => {
+      return context.prisma.links()
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
+    post: (root, args, context) => {
+      return context.prisma.createLink({
         url: args.url,
-      }
-
-      links.push(link)
-      return link
-    },
-    updateLink: (parent, args) => {
-      let newLinks = cloneDeep(links);
-      let result;
-      links = newLinks.map((link) => {
-        if(link.id === args.id) {
-          link.url = args.url
-          link.description = args.description
-
-          result = link
-        }
-        return link
-      });
-
-      links = newLinks;
-
-      return result;
-    },
-    deleteLink: (parent, args) => {
-      const removeLink = links.find((link) => {
-        return (link.id === args.id)
+        description: args.description,
       })
+    },
+    // updateLink: (parent, args) => {
+    //   let newLinks = cloneDeep(links);
+    //   let result;
+    //   links = newLinks.map((link) => {
+    //     if(link.id === args.id) {
+    //       link.url = args.url
+    //       link.description = args.description
 
-      links = links.filter((link) => {
-        return (link.id !== args.id)
-      });
+    //       result = link
+    //     }
+    //     return link
+    //   });
 
-      return removeLink
-    }
+    //   links = newLinks;
+
+    //   return result;
+    // },
+    // deleteLink: (parent, args) => {
+    //   const removeLink = links.find((link) => {
+    //     return (link.id === args.id)
+    //   })
+
+    //   links = links.filter((link) => {
+    //     return (link.id !== args.id)
+    //   });
+
+    //   return removeLink
+    // }
   }
 }
 
 const server = new GraphQLServer({
   typeDefs: './src/schema.graphql',
   resolvers,
+  context: { prisma },
 })
 server.start(() => console.log(`Server is running on http://localhost:4000`))
