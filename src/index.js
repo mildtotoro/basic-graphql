499110c31d8dae003db55b021f2a41285ffe36c1
mildtotoro <pimrampha.m@gmail.com>
2019-06-04 12:47:40 +0700

    Subscription link

--- src/index.js
+++ src/index.js
@@ -6,18 +6,19 @@ const Query = require('./resolvers/Query')
 const Mutation = require('./resolvers/Mutation')
 const User = require('./resolvers/User')
 const Link = require('./resolvers/Link')
+const Subscription = require('./resolvers/Subscription')
 
 const resolvers = {
   Query,
   Mutation,
+  Subscription,
   User,
-  Link
+  Link,
 }
-
 
 //https://www.howtographql.com/graphql-js/6-authentication/
 const server = new GraphQLServer({
-  typeDefs: './schema.graphql',
+  typeDefs: './src/schema.graphql',
   resolvers,
   context: request => { 
     return {
